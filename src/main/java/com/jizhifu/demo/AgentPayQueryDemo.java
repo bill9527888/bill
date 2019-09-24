7b3a1d1315c3c7045e63a0a7954b936f84feb2a3
380907395 <380907395@qq.com>
2019-09-24 11:37:34 +0800

    修改备注

--- src/main/java/com/jizhifu/demo/AgentPayQueryDemo.java
+++ src/main/java/com/jizhifu/demo/AgentPayQueryDemo.java
@@ -18,8 +18,8 @@ public class AgentPayQueryDemo {
 
     private static String appKey = "c3246abfcddb46f3a26c845f06b8a293";  //appKey(商户后台设置可以获取到值)
     private static String appSecret = "a62e17090db0e808a6ab3675a41bc06b"; //appSecret 为系统后台安全设置的 appSecret 值
-    //private static String urlPref = "http://pay-test.cpayxx.com/api/exchange";//测试环境
-    private static String urlPref = "http://api.jipay.io/exchange";//正式环境(备注：请以接口文档的地址为准)
+    private static String urlPref = "http://pay-test.cpayxx.com/api/exchange";//测试环境
+    //private static String urlPref = "http://api.jipay.io/exchange";//正式环境(备注：请以接口文档的地址为准)
 
     public static void main(String[] args) throws Exception {
         String url = urlPref + "/api/open/agentPay/queryOrder";
